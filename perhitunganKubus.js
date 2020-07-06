aba5c3c41f263c4609493039695832de0fd7636b
ariwisnu <wisnuari4658@gmail.com>
2020-07-07 00:33:49 +0800

    menambahkan parseint

--- perhitunganKubus.js
+++ perhitunganKubus.js
@@ -1,6 +1,6 @@
 function jumnlahVolume(a,b) {
     var  volume1= a * a * a;
-    var volume2= b * b *b;
+    var  volume2= b * b *b;
     var hasil = volume1 + volume2;
 
 return hasil;
@@ -10,8 +10,8 @@ var confirm = true;
 
 
 while (confirm == true) {
-    var sisiKubus1 = prompt('masukan sisi kubus pertama');
-    var sisiKubus2 = prompt('masukan sisi kubus kedua');
+    var sisiKubus1 = parseInt(prompt('masukan sisi kubus pertama'));
+    var sisiKubus2 = parseInt(prompt('masukan sisi kubus kedua'));
     
     alert(jumnlahVolume(sisiKubus1,sisiKubus2));
     
