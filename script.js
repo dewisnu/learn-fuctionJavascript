084c51e0bb1bb638cbe459618cd96179538d70bb
ariwisnu <wisnuari4658@gmail.com>
2020-07-06 17:10:24 +0800

    menambahkan fuction penjumblahDuaBilangan dan pengurang DuaBilangan

---
+++ script.js
@@ -0,0 +1,15 @@
+function jumblahDuaBilangan(a,b) {
+    var jumblah = a + b;
+    
+    return jumblah;
+}
+
+var penguranganDuaBilangan = function (a,b) {
+    
+    mengurang = a - b;
+
+    return mengurang;
+}
+
+alert(jumblahDuaBilangan(1,2));
+alert(penguranganDuaBilangan(2,1));
